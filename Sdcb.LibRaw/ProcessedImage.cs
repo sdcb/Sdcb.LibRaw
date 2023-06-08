fb21a662dd0dc51e203696b88e6c0d4bd043ccfe
sdcb <sdflysha@qq.com>
2023-06-08 17:43:26 +0800

    Add DataPtr to ProcessedImage.

--- Sdcb.LibRaw/ProcessedImage.cs
+++ Sdcb.LibRaw/ProcessedImage.cs
@@ -10,9 +10,7 @@ public unsafe class ProcessedImage : IDisposable
 {
     private LibRawProcessedImage* _image;
 
-    /// <summary>
-    /// Initializes a new instance of the <see cref="ProcessedImage"/> class.
-    /// </summary>
+    /// <summary>Initializes a new instance of the <see cref="ProcessedImage"/> class.</summary>
     /// <param name="image">The LibRaw processed image.</param>
     public ProcessedImage(LibRawProcessedImage* image)
     {
@@ -26,9 +24,7 @@ public unsafe class ProcessedImage : IDisposable
         GC.SuppressFinalize(this);
     }
 
-    /// <summary>
-    /// Cleanup the managed and unmanaged resources used.
-    /// </summary>
+    /// <summary>Cleanup the managed and unmanaged resources used.</summary>
     /// <param name="disposing">Flag indicating if <see cref="_image"/> should be disposed.</param>
     protected virtual void Dispose(bool disposing)
     {
@@ -41,55 +37,40 @@ public unsafe class ProcessedImage : IDisposable
         }
     }
 
-    /// <summary>
-    /// Frees the memory used by this instance.
-    /// </summary>
+    /// <summary>Frees the memory used by this instance.</summary>
     private void FreeImage()
     {
         LibRawNative.ClearDcrawMemory((IntPtr)_image);
         _image = null;
     }
 
-    /// <summary>
-    /// Gets a value indicating whether this instance has been disposed.
-    /// </summary>
+    /// <summary>Gets a value indicating whether this instance has been disposed.</summary>
     public bool Disposed => _image == null;
 
-    /// <summary>
-    /// Gets the image format.
-    /// </summary>
+    /// <summary>Gets the image format.</summary>
     public ProcessedImageType ImageType => _image->Type;
 
-    /// <summary>
-    /// Gets the image height.
-    /// </summary>
+    /// <summary>Gets the image height.</summary>
     public int Height => _image->Height;
 
-    /// <summary>
-    /// Gets the image weight.
-    /// </summary>
+    /// <summary>Gets the image weight.</summary>
     public int Width => _image->Width;
 
-    /// <summary>
-    /// Gets the number of colors in the image.
-    /// </summary>
+    /// <summary>Gets the number of colors in the image.</summary>
     public int Colors => _image->Colors;
 
-    /// <summary>
-    /// Gets the bits value of the image.
-    /// </summary>
+    /// <summary>Gets the bits value of the image.</summary>
     public int Bits => _image->Bits;
 
-    /// <summary>
-    /// Gets the data as an array-like <see cref="Span{T}"/> object.
-    /// </summary>
+    /// <summary>Gets the image data ptr.</summary>
+    public IntPtr DataPtr => (IntPtr)(&_image->FirstData);
+
+    /// <summary>Gets the data as an array-like <see cref="Span{T}"/> object.</summary>
     /// <typeparam name="T">The type of the data.</typeparam>
     /// <returns>The data in a <see cref="Span{T}"/> object.</returns>
     public Span<T> GetData<T>() => _image->GetData<T>();
 
-    /// <summary>
-    /// Allows the finalizer to free memory used by this instance.
-    /// </summary>
+    /// <summary>Allows the finalizer to free memory used by this instance.</summary>
     ~ProcessedImage()
     {
         Dispose(false);
